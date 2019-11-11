4cf6ad893c6f569eb1aa3438a1868c95c28b911d
Joe Ciccone <ciccone.n.joseph@gmail.com>
2019-11-11 17:48:50 -0500

    created nav bar using router functionality added bootstrap funcitonality for css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContactComponent } from './contact/contact.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HistoryComponent } from './history/history.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContactComponent,
+    SignUpComponent,
+    HistoryComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
