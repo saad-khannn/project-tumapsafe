d13167db1016b74a82a43e140e5781fb13b6fe4e
MeetPatel72 <tuf64734@templedu>
2019-11-30 01:58:33 -0500

    added help

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HistoryComponent } from './history/history.component';
+import { HelpComponent } from './help/help.component';
+
 import { HomeComponent } from './home/home.component';
+
 import { SignUpComponent } from './sign-up/sign-up.component';
+
 
 
 const routes: Routes = [
   {path: 'contact', component: ContactComponent},
   {path: 'history', component: HistoryComponent},
+  {path: 'help', component: HelpComponent},
   {path: '', component: HomeComponent},
   {path: 'signUp', component: SignUpComponent}
+
 ];
 
 @NgModule({
