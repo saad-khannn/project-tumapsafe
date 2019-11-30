4be88c56138044d87dc4240e677a15bab647bef8
Saad <49029610+saad-khannn@users.noreply.github.com>
2019-11-30 08:39:58 -0500

    get description and location of most recent alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  Alerts: any[]
 
-  constructor(db: AngularFireDatabase) { 
-    db.list('/Alerts').valueChanges().subscribe(Alerts =>{
-      this.Alerts = Alerts;
-      console.log(this.Alerts);
-    })
-  }
+export class AppComponent {
+  title = 'tu-mapsafe';
 }
